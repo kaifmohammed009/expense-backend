1441a7f1990b39b4b48c75c7d19990e059fa8b24
kaifmohammed009 <smohamedkaif6@gmail.com>
2026-02-17 23:36:39 +0530

    final fix router middleware

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json());
-const expenseRoutes = require ("./routes/expenseRoutes");
-app.use("/api/expenses",("./routes/expenseRoutes"));
-app.listen(5000, () => console.log("Server running on port 5000"));
+const expenseRoutes = require("./routes/expenseRoutes");
+app.use("/api/expenses", expenseRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
